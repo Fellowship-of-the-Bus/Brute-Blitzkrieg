3da2eb2e367dbdc70778b521bb1e4ef721af65be
Kevin Wu <kevin.kaiyu.wu@gmail.com>
2016-03-12 12:09:43 -0500

    added example map for testing

--- src/main/scala/models/Map.scala
+++ src/main/scala/models/Map.scala
@@ -7,23 +7,38 @@ import scala.collection.mutable.Set
 
 
 case class Tile(val floorTrapID: Int, val wallTrapID: Int) {
-	val bruteList: Set[Brute] = Set[Brute]()
+  val bruteList: Set[Brute] = Set[Brute]()
 
-	def register(e: Brute) = {
+  def register(e: Brute) = {
         bruteList += e
     }
 
-    def deregister(e: Brute) = {
-        bruteList -= e
-    }
+  def deregister(e: Brute) = {
+      bruteList -= e
+  }
 }
 
 case class MapInfo(
-	tiles: List[List[Tile]],
-	startTileCoord: (Int, Int),
-	endTileCoord: (Int, Int))
+  tiles: List[List[Tile]],
+  startTileCoord: (Int, Int),
+  endTileCoord: (Int, Int))
 
-class Map(num : Int) {
-	val brute = new Brute()
+// tiles have coordinates with bottom left = (0,0)
+class LevelMap() {
+  def apply(num: Int) = levelMap(num)
+  val levelMap = collection.immutable.HashMap(1 -> 
+  MapInfo(List(List(Tile(0,0), Tile(0,0), Tile(0,0), Tile(0,0), Tile(0,0)),
+               List(Tile(0,0), Tile(0,0), Tile(0,0), Tile(0,0), Tile(0,0))
+               ),
+          (0,4), (1,4)
+          )
+  )
+}
 
+class GameMap(num : Int) {
+  val brute = new Brute()
+  val m = new LevelMap()
+  val tiles = m(num).tiles
+  val startTileCoord = m(num).startTileCoord
+  val endTileCoord = m(num).endTileCoord
 }
