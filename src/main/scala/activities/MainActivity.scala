666b2b2fc89070487b70d6be460f472d4f98cb33
Travis Bartlett <trbartle@uwaterloo.ca>
2016-03-12 17:31:08 -0500

    Stub for main screen

--- src/main/scala/activities/MainActivity.scala
+++ src/main/scala/activities/MainActivity.scala
@@ -2,12 +2,12 @@ package com.github.fellowship_of_the_bus.bruteb
 
 import models._
 
-
 // import android.app.Activity
 import org.scaloid.common._
 import android.os.Bundle
 import android.view.Gravity
 import android.graphics.Color
+import android.widget.GridView
 import java.util.concurrent.atomic.AtomicInteger
 
 import scala.language.postfixOps
@@ -18,20 +18,51 @@ class MainActivity extends SActivity {
   override def onCreate(savedState: Bundle) {
     super.onCreate(savedState)
     val prefs = Preferences()
-
     viewSeq.set(0)
     setContentView(
-      new SVerticalLayout {
-        style {
-          case v => v.id = viewSeq.incrementAndGet
-        }
-        STextView("Say Hello").gravity(Gravity.CENTER).textSize(16 sp).textColor(Color.BLUE).<<.marginBottom(16 dip).>>
-        val name = SEditText(prefs.String.name.getOrElse("No name")).hint("enter your first name").selectAllOnFocus(true)
-        SButton("Greet!", {
-          val who = name.text.toString
-          prefs.name = who
-          alert(telephonyManager.getLine1Number + " Welcomes You", "Hello " + who)
-        })
+
+      new SLinearLayout {
+        new STableLayout {
+          this += new STableRow {
+            SButton("Level One").<<.wrap.>>
+            SButton("Level Two").<<.wrap.>>
+            SButton("Level Three").<<.wrap.>>
+          }
+          this += new STableRow {
+            SButton("Level Four").<<.wrap.>>
+            SButton("Level Five").<<.wrap.>>
+            SButton("Level Six").<<.wrap.>>
+          }
+          this += new STableRow {
+            SButton("Level Seven").<<.wrap.>>
+            SButton("Level Eight").<<.wrap.>>
+            SButton("Level Nine").<<.wrap.>>
+          }
+          this += new STableRow {
+            SButton("Level Ten").<<.wrap.>>
+            SButton("Level Eleven").<<.wrap.>>
+            SButton("Level Twelve").<<.wrap.>>
+          }
+        }.<<.wrap.>>.here//.stretchColumns("*")
+        /*var grid = new SGridLayout {
+          SButton("Level One").<<.wrap.>>
+          SButton("Level Two").<<.wrap.>>
+          SButton("Level Three").<<.wrap.>>
+          SButton("Level Four").<<.wrap.>>
+          SButton("Level Five").<<.wrap.>>
+          SButton("Level Six").<<.wrap.>>
+          SButton("Level Seven").<<.wrap.>>
+          SButton("Level Eight").<<.wrap.>>
+          SButton("Level Nine").<<.wrap.>>
+          SButton("Level Ten").<<.wrap.>>
+          SButton("Level Eleven").<<.wrap.>>
+          SButton("Level Twelve").<<.wrap.>>
+        }.<<.wrap.>>.columnCount(3).columnOrderPreserved(true).here*/
+        new SRelativeLayout {
+          SButton("Minions").<<.wrap.alignParentTop.>>
+          SButton("Traps").<<.wrap.centerVertical.>>
+          SButton("Quit").<<.wrap.alignParentBottom.>>
+        }.<<.fw.>>.gravity(Gravity.RIGHT).here
       }
     )
   }
