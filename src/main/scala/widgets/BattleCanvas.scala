72f379308380ab2b6e994b0d25f25eb36266a735
Rob <rschlunt@uwaterloo.ca>
2016-03-19 17:30:10 -0400

    draw Ahmed's eyes on the canvas

--- src/main/scala/widgets/BattleCanvas.scala
+++ src/main/scala/widgets/BattleCanvas.scala
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.BitmapFactory
 import android.os.Bundle;
 import android.view.View;
 
@@ -23,5 +24,14 @@ class BattleCanvas(implicit context: Context) extends SView {
     // Use Color.parseColor to define HTML colors
     paint.setColor(Color.parseColor("#CD5C5C"));
     canvas.drawCircle(x / 2, y / 2, radius, paint);
+
+    val b = BitmapFactory.decodeResource(getResources(), R.drawable.ahmed);
+    canvas.save()
+    canvas.scale(5.5f, 5.5f)
+    canvas.drawBitmap(b, 0, 0, null);
+    canvas.restore()
+
+    paint.setColor(Color.WHITE);
+    canvas.drawRect(0,0, getWidth(), 2*getHeight()/3,paint);
   }
 }
