05b9c55a93f28a71b21ac38a462016fac53ac2f2
Rob Schluntz <rschlunt@uwaterloo.ca>
2016-04-16 16:42:03 -0400

    draw lifebars

--- src/main/scala/widgets/BattleCanvas.scala
+++ src/main/scala/widgets/BattleCanvas.scala
@@ -21,16 +21,16 @@ import android.os.{Handler, Message}
 
 
 import models.MapInfo
-import models.{BruteID, TrapID, Game}
+import models.{BruteID, TrapID, Game, BaseBrute}
 
 object BattleCanvas {
   var canvas : BattleCanvas = null
   var decoderOptions = new BitmapFactory.Options()
   decoderOptions.inSampleSize = 8
   lazy val backgroundImage = BitmapFactory.decodeResource(canvas.getResources(), R.drawable.map);
-  lazy val bruteImages: Map[BruteID, Bitmap] = (for (x <- BruteID.Factory.ids) yield 
+  lazy val bruteImages: Map[BruteID, Bitmap] = (for (x <- BruteID.Factory.ids) yield
     (x, BitmapFactory.decodeResource(canvas.getResources(), x.image, decoderOptions))).toMap
-  lazy val trapImages: Map[TrapID, Bitmap] = (for (x <- TrapID.Factory.ids ++ TrapID.Factory.openIds) yield 
+  lazy val trapImages: Map[TrapID, Bitmap] = (for (x <- TrapID.Factory.ids ++ TrapID.Factory.openIds) yield
     (x, BitmapFactory.decodeResource(canvas.getResources(), x.image, decoderOptions))).toMap
 }
 
@@ -72,6 +72,8 @@ class BattleCanvas(val map: MapInfo)(implicit context: Context) extends SView {
     }
     for (brute <- Game.game.bruteList.filter(_.isAlive)) {
       //to do climbing stairs
+
+      drawLifebar(brute)
       val image = bruteImages(brute.id)
       android.util.Log.e("bruteb", "Draw brute at " + brute.x.toString + " " + brute.y.toString)
       drawPositioned(image, brute)
@@ -90,6 +92,32 @@ class BattleCanvas(val map: MapInfo)(implicit context: Context) extends SView {
     def drawPositioned(image: Bitmap, gameObject: TopLeftCoordinates) = {
       canvas.drawBitmap(image, null, new Rect(normX(gameObject.x), normY(gameObject.y),
                     normX(gameObject.x + gameObject.width), normY(gameObject.y + gameObject.height)), null)
+    }
+    def drawLifebar(brute: BaseBrute) = {
+      // vertical distance between a lifebar and the top left corner of the image
+      val dist = 10
+      val hp = brute.hp
+      val maxHp = brute.maxHP
+      val (left, top, right, _) = brute.coordinates
+      val width = right-left
+      val height = 5
+
+      val x1 = normX(left)
+      val x2 = normX(right)
+      val y1 = normY(top)-dist
+      val y2 = y1-height
+
+      val pos = new Rect(x1, y1, x2, y2)
+      val paint = new Paint();
+      paint.setStyle(Paint.Style.STROKE)
+      paint.setColor(Color.BLACK)
+      canvas.drawRect(pos, paint)
+
+      pos.set(x1, y1, normX(left+width*hp/maxHp), y2)
+      paint.setStyle(Paint.Style.FILL)
+      paint.setColor(Color.RED)
+      canvas.drawRect(pos, paint)
+      error(s"drawing lifebar at $pos")(new LoggerTag("bruteb"))
     }
     battleHandler.sleep(100)
   }
