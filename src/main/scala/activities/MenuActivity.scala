04b427faf0440efa80c35c54a02acf8154a5a76a
Travis Bartlett <trbartle@uwaterloo.ca>
2016-08-20 16:35:10 -0400

    Updated main menu

--- src/main/scala/activities/MenuActivity.scala
+++ src/main/scala/activities/MenuActivity.scala
@@ -6,7 +6,7 @@ import models._
 import org.scaloid.common._
 import android.os.Bundle
 import android.view.Gravity
-import android.graphics.Color
+import android.graphics.{Color, Point}
 import android.graphics.drawable.{Drawable,BitmapDrawable, LayerDrawable}
 import android.widget.GridView
 import android.content.{Intent, Context}
@@ -26,31 +26,37 @@ class MenuActivity extends BaseActivity {
   override def onCreate(savedState: Bundle) {
     super.onCreate(savedState)
     Game.res = getResources
+    val point = new Point()
+    getWindowManager().getDefaultDisplay().getSize(point)
 
     setContentView(
 
       // number of stars earned in level i
       new SRelativeLayout {
-          new STableLayout {
-            new STableRow {
-              SButton(R.string.LevelSelectButton, switchScreen(classOf[MainActivity],false)).<<.fw.>>
-            }.<<.wrap.>>.here
-            new STableRow {
-              SButton(R.string.OptionButton, switchScreen(classOf[OptionActivity],false)).<<.fw.>>
-            }.<<.wrap.>>.here
-            new STableRow {
-              SButton(R.string.LevelEditorButton, switchScreen(classOf[LevelEditor],false)).<<.fw.>>
-            }.<<.wrap.>>.here
-            new STableRow {
-              SButton(R.string.BruteButton, switchScreen(classOf[Encyclopedia],true)).<<.fw.>>
-            }.<<.wrap.>>.here
-            new STableRow {
-              SButton(R.string.TrapButton, switchScreen(classOf[Encyclopedia],false)).<<.fw.>>
-            }.<<.wrap.>>.here
-           // new STableRow {
-           //   SImageView(R.drawable.logo)
-           // }.<<.wrap.>>.here
-          }.<<.fill.>>.gravity(Gravity.CENTER_HORIZONTAL).here
+          new SVerticalLayout {
+            SImageView(R.drawable.brute_blitzkrieg).<<(point.x/2,WRAP_CONTENT).>>.adjustViewBounds(true)
+            new SLinearLayout {
+              new SView{}.<<(0,WRAP_CONTENT).Weight(4).>>.here
+              new STableLayout {
+                new STableRow {
+                  SButton(R.string.LevelSelectButton, switchScreen(classOf[MainActivity],false)).<<.fw.>>
+                }.<<.wrap.>>.here
+                new STableRow {
+                  SButton(R.string.OptionButton, switchScreen(classOf[OptionActivity],false)).<<.fw.>>
+                }.<<.wrap.>>.here
+                new STableRow {
+                  SButton(R.string.LevelEditorButton, switchScreen(classOf[LevelEditor],false)).<<.fw.>>
+                }.<<.wrap.>>.here
+                new STableRow {
+                  SButton(R.string.BruteButton, switchScreen(classOf[Encyclopedia],true)).<<.fw.>>
+                }.<<.wrap.>>.here
+                new STableRow {
+                  SButton(R.string.TrapButton, switchScreen(classOf[Encyclopedia],false)).<<.fw.>>
+                }.<<.wrap.>>.here
+              }.<<(0,WRAP_CONTENT).Weight(1).>>.gravity(Gravity.CENTER_HORIZONTAL).here
+              new SView{}.<<(0,WRAP_CONTENT).Weight(1).>>.here
+            }.here
+          }.<<.fill.marginTop(10 dip).>>.gravity(Gravity.CENTER_HORIZONTAL).here
       }.background(R.drawable.mainsplash)
     )
   }
